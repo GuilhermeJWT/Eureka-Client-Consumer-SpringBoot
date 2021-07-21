c975f04200f7dfcbf1b0b016bab0243ae7b93c71
Guilherme Santos <guiromanno@gmail.com>
2021-07-21 18:53:32 -0300

    Registrando no Eureka Server

--- src/main/java/br/com/systemsgs/ConsumerServiceApplication.java
+++ src/main/java/br/com/systemsgs/ConsumerServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.systemsgs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsumerServiceApplication {
 
 	public static void main(String[] args) {
