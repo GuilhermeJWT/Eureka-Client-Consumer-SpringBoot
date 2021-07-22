103c30892433198309530b9935578afa25eafdaf
Guilherme Santos <guiromanno@gmail.com>
2021-07-22 11:38:09 -0300

    Adicionando e Habilitando o OpenFeign para fazer Integração com Payment-Service

--- src/main/java/br/com/systemsgs/ConsumerServiceApplication.java
+++ src/main/java/br/com/systemsgs/ConsumerServiceApplication.java
@@ -3,9 +3,11 @@ package br.com.systemsgs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class ConsumerServiceApplication {
 
 	public static void main(String[] args) {
